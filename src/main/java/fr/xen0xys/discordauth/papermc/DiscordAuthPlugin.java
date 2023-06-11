1b8a1142932cfd8224ab1e154c27966503c9fbbc
Czekaj Tom <red57101@gmail.com>
2023-06-11 15:30:15 +0200

    :rocket: Add main plugins classes

--- src/main/java/fr/xen0xys/discordauth/papermc/DiscordAuthPlugin.java
+++ src/main/java/fr/xen0xys/discordauth/papermc/DiscordAuthPlugin.java
@@ -1,25 +1,44 @@
 package fr.xen0xys.discordauth.papermc;
 
+import fr.xen0xys.discordauth.common.PluginInfos;
+import fr.xen0xys.discordauth.papermc.events.OnPlayerJoin;
+import fr.xen0xys.discordauth.papermc.events.OnPluginMessage;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class DiscordAuthPlugin extends JavaPlugin {
 
+    private static DiscordAuthPlugin instance;
     private static Logger logger;
+    private static final List<Player> connectedPlayers = new ArrayList<>();
 
     @Override
     public void onLoad() {
+        instance = this;
         logger = this.getLogger();
     }
 
     @Override
     public void onEnable() {
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, PluginInfos.CHANNEL);
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, PluginInfos.CHANNEL, new OnPluginMessage());
+        this.getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
         logger.info("DiscordAuth is started !");
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    public static DiscordAuthPlugin getInstance() {
+        return instance;
+    }
+    public static List<Player> getConnectedPlayers() {
+        return connectedPlayers;
     }
 }
--- src/main/java/fr/xen0xys/discordauth/waterfall/DiscordAuthProxy.java
+++ src/main/java/fr/xen0xys/discordauth/waterfall/DiscordAuthProxy.java
@@ -1,25 +1,35 @@
 package fr.xen0xys.discordauth.waterfall;
 
+import fr.xen0xys.discordauth.common.PluginInfos;
+import fr.xen0xys.discordauth.waterfall.events.OnPluginMessage;
 import net.md_5.bungee.api.plugin.Plugin;
 
 import java.util.logging.Logger;
 
 public class DiscordAuthProxy extends Plugin {
 
+    private static DiscordAuthProxy instance;
     private static Logger logger;
 
     @Override
     public void onLoad() {
+        instance = this;
         logger = this.getLogger();
     }
 
     @Override
     public void onEnable() {
+        this.getProxy().registerChannel(PluginInfos.CHANNEL);
+        getProxy().getPluginManager().registerListener(this, new OnPluginMessage());
         logger.info("DiscordAuth is started !");
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    public static DiscordAuthProxy getInstance() {
+        return instance;
     }
 }
