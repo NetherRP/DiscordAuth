9ab2fcdec1acd23528810846f4b8c225a77aa4dd
Czekaj Tom <red57101@gmail.com>
2023-06-11 23:25:26 +0200

    :bug: Remove username and isOnline field from packets (only work with online and offline uuid)

--- src/main/java/fr/xen0xys/discordauth/common/network/packets/SessionAskPacket.java
+++ src/main/java/fr/xen0xys/discordauth/common/network/packets/SessionAskPacket.java
@@ -5,30 +5,13 @@ import fr.xen0xys.discordauth.common.network.Packet;
 import java.util.UUID;
 
 public class SessionAskPacket extends Packet {
-    private final boolean onlineMode;
-    private final String username;
+
     private final UUID uuid;
 
-    public SessionAskPacket(boolean onlineMode, String username, UUID uuid) {
-        this.onlineMode = onlineMode;
-        this.username = username;
+    public SessionAskPacket(UUID uuid) {
         this.uuid = uuid;
     }
 
-    public SessionAskPacket(String username) {
-        this(true, username, null);
-    }
-
-    public SessionAskPacket(UUID uuid) {
-        this(true, null, uuid);
-    }
-
-    public boolean isOnlineMode() {
-        return onlineMode;
-    }
-    public String getUsername() {
-        return username;
-    }
     public UUID getUuid() {
         return uuid;
     }
