b19dbd33a8b5bc02434d3ee72cfda82d3fc4d1ea
Czekaj Tom <red57101@gmail.com>
2023-06-12 00:48:10 +0200

    :sparkles: Implement discord bot

---
+++ src/main/java/fr/xen0xys/discordauth/common/discord/Bot.java
@@ -0,0 +1,29 @@
+package fr.xen0xys.discordauth.common.discord;
+
+import fr.xen0xys.discordauth.common.config.Configuration;
+import fr.xen0xys.discordauth.common.discord.components.buttons.RegisterButton;
+import fr.xen0xys.discordauth.common.discord.components.commands.DisplayRegisterCommand;
+import fr.xen0xys.discordjava.DJApp;
+import net.dv8tion.jda.api.entities.Guild;
+
+import java.util.Objects;
+
+public class Bot {
+
+    public Bot(Configuration configuration){
+        this(configuration.getBotToken());
+    }
+
+    public Bot(String token){
+        try {
+            DJApp app = new DJApp(token, "DiscordAuthBot");
+//            new DisplayRegisterCommand().register(this.app.getCommandsManager(), true);
+            Guild guild = app.getJDA().getGuildById(1016358670600245369L);
+            if(Objects.isNull(guild)) return;
+            new DisplayRegisterCommand().registerLocal(app.getCommandsManager(), guild, true);
+            new RegisterButton().register(app.getComponentsManager());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/fr/xen0xys/discordauth/waterfall/DiscordAuthProxy.java
+++ src/main/java/fr/xen0xys/discordauth/waterfall/DiscordAuthProxy.java
@@ -3,11 +3,10 @@ package fr.xen0xys.discordauth.waterfall;
 import fr.xen0xys.discordauth.common.PluginInfos;
 import fr.xen0xys.discordauth.common.config.Configuration;
 import fr.xen0xys.discordauth.common.database.DatabaseHandler;
-import fr.xen0xys.discordauth.common.database.models.Account;
+import fr.xen0xys.discordauth.common.discord.Bot;
 import fr.xen0xys.discordauth.waterfall.events.OnPluginMessage;
 import net.md_5.bungee.api.plugin.Plugin;
 
-import java.util.UUID;
 import java.util.logging.Logger;
 
 public class DiscordAuthProxy extends Plugin {
@@ -16,6 +15,7 @@ public class DiscordAuthProxy extends Plugin {
     private static Logger logger;
     private static Configuration configuration;
     private static DatabaseHandler databaseHandler;
+    private static Bot bot;
 
     @Override
     public void onLoad() {
@@ -23,6 +23,15 @@ public class DiscordAuthProxy extends Plugin {
         logger = this.getLogger();
         configuration = new Configuration(this.getDataFolder(), "config.yml");
         databaseHandler = new DatabaseHandler(configuration.getSessionFactory());
+
+        // TODO: temporary
+//        UUID uuid = UUID.randomUUID();
+//        databaseHandler.addAccount(new Account(uuid, "Username", "Password", "LastIp", 0L));
+//        logger.info("Added account with UUID " + uuid);
+//        boolean state = databaseHandler.isAccountExists(uuid);
+//        logger.info("Account with UUID " + uuid + " exists: " + state);
+
+        bot = new Bot(configuration);
     }
 
     @Override
