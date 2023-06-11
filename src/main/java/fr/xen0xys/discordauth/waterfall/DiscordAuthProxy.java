688156d73abdc38fcd411b9826698399ce2435f0
Czekaj Tom <red57101@gmail.com>
2023-06-11 23:26:24 +0200

    :rocket: Implement Hibernate into BungeeCord plugin

--- src/main/java/fr/xen0xys/discordauth/waterfall/DiscordAuthProxy.java
+++ src/main/java/fr/xen0xys/discordauth/waterfall/DiscordAuthProxy.java
@@ -2,9 +2,12 @@ package fr.xen0xys.discordauth.waterfall;
 
 import fr.xen0xys.discordauth.common.PluginInfos;
 import fr.xen0xys.discordauth.common.config.Configuration;
+import fr.xen0xys.discordauth.common.database.DatabaseHandler;
+import fr.xen0xys.discordauth.common.database.models.Account;
 import fr.xen0xys.discordauth.waterfall.events.OnPluginMessage;
 import net.md_5.bungee.api.plugin.Plugin;
 
+import java.util.UUID;
 import java.util.logging.Logger;
 
 public class DiscordAuthProxy extends Plugin {
@@ -12,12 +15,14 @@ public class DiscordAuthProxy extends Plugin {
     private static DiscordAuthProxy instance;
     private static Logger logger;
     private static Configuration configuration;
+    private static DatabaseHandler databaseHandler;
 
     @Override
     public void onLoad() {
         instance = this;
         logger = this.getLogger();
         configuration = new Configuration(this.getDataFolder(), "config.yml");
+        databaseHandler = new DatabaseHandler(configuration.getSessionFactory());
     }
 
     @Override
@@ -37,5 +42,8 @@ public class DiscordAuthProxy extends Plugin {
     }
     public static Configuration getConfiguration() {
         return configuration;
+    }
+    public static DatabaseHandler getDatabaseHandler() {
+        return databaseHandler;
     }
 }
